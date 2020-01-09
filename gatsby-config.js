3130efdda124da1212e92fd91758d1bef4d0e11c
Kaleigh Scruggs <kaleigh.scruggs@northhighland.com>
2020-01-09 18:06:48 -0500

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
         background_color: `white`,
         theme_color: `black`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        /* icon: `src/images/gatsby-icon.png`,  */
       },
     },
     {
