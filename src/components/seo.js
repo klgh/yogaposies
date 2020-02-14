1b8c546176b0a7b1b8e843961d3f6e40693383b7
Kaleigh Scruggs <kaleigh.scruggs@northhighland.com>
2020-02-14 11:18:41 -0500

    some more SEO

--- src/components/seo.js
+++ src/components/seo.js
@@ -20,6 +20,7 @@ function SEO({ description, lang, meta, title }) {
             description
             author
             shortDescription
+            siteUrl
           }
         }
       }
@@ -51,6 +52,10 @@ function SEO({ description, lang, meta, title }) {
         {
           property: `og:type`,
           content: `website`,
+        },
+        {
+          property: `og:url`,
+          content: site.siteMetadata.siteUrl,
         },
         {
           name: `twitter:card`,
