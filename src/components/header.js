197dac10e81a1b5b3d5437c784695df22a94b657
Kaleigh Scruggs <kaleigh.scruggs@northhighland.com>
2020-02-17 16:47:36 -0500

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -13,67 +13,65 @@ const Header = ({ siteTitle }) => (
         <Link>Yoga Posies</Link>
       </h1>
     </div>
-    <nav role="navigation">
-      <div className="ypMenu" id="menuToggle">
-        <ul className="header-links" id="menu">
-          <li>
-            <Link
-              to="/about"
-              className="linkItem"
-              activeClassName="linkItemActive"
-            >
-              about
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/classes"
-              className="linkItem"
-              activeClassName="linkItemActive"
-            >
-              classes
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/summer-camp"
-              className="linkItem"
-              activeClassName="linkItemActive"
-            >
-              summer camp
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/forms"
-              className="linkItem"
-              activeClassName="linkItemActive"
-            >
-              forms
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/contact"
-              className="linkItem"
-              activeClassName="linkItemActive"
-            >
-              contact
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="https://www.facebook.com/YogaPosies/"
-              className="font-awesome-icon"
-            >
-              <FontAwesomeIcon
-                title="yoga posies facebook"
-                icon={["fab", "facebook"]}
-              />
-            </Link>
-          </li>
-        </ul>
-      </div>
+    <nav role="navigation" className="ypMenu">
+      <ul className="header-links" id="menu">
+        <li>
+          <Link
+            to="/about"
+            className="linkItem"
+            activeClassName="linkItemActive"
+          >
+            about
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/classes"
+            className="linkItem"
+            activeClassName="linkItemActive"
+          >
+            classes
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/summer-camp"
+            className="linkItem"
+            activeClassName="linkItemActive"
+          >
+            summer camp
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/forms"
+            className="linkItem"
+            activeClassName="linkItemActive"
+          >
+            forms
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/contact"
+            className="linkItem"
+            activeClassName="linkItemActive"
+          >
+            contact
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="https://www.facebook.com/YogaPosies/"
+            className="font-awesome-icon"
+          >
+            <FontAwesomeIcon
+              title="yoga posies facebook"
+              icon={["fab", "facebook"]}
+            />
+          </Link>
+        </li>
+      </ul>
     </nav>
   </header>
 )
