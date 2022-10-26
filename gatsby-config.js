fb51fbe21ffaccd3275e01e72b9eb0032460059f
Kaleigh Scruggs <kaleighscruggs@gmail.com>
2022-10-26 14:29:16 -0700

    some stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,45 +1,40 @@
-require("dotenv").config()
-const path = require(`path`)
-
 module.exports = {
   siteMetadata: {
-    title: `Yoga Posies`,
-    description: `Yoga Classes in the metro Atlanta area including Dunwoody, Roswell, and Sandy Springs. Classes are geared towards anyone and everyone including kids, pre-teens, teens, adults, and prenatal yoga.`,
-    author: `@kaleighscruggs`,
-    siteUrl: "https://yogaposies.com",
-    shortDescription: `Our goal at Yoga Posies is to introduce yoga in a clear and simple way that makes it fun to do.`,
-    image: `src/images/YogaPosies.png`,
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sitemap`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-sass`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-sitemap`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Yoga Posies`,
-        short_name: `yoga posies`,
-        start_url: `/`,
-        background_color: `#f5f5f5`,
-        theme_color: `#a89797`,
-        display: `minimal-ui`,
-        icon: `src/images/favicon.png`,
-      },
-    },
+    `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: path.join(__dirname, `src`, `images`),
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     {
-      resolve: "gatsby-source-wordpress",
+      resolve: `gatsby-source-wordpress`,
       options: {
+        // the only required plugin option for WordPress is the GraphQL url.
+        url: `https://content.yogaposies.com/graphql`,
         baseUrl: process.env.WORDPRESS_BASE_URL,
         protocol: process.env.WORDPRESS_PROTOCOL,
         hostingWPCOM: process.env.WORDPRESS_HOSTING_WPCOM === "true",
@@ -47,63 +42,10 @@ module.exports = {
         acfOptionPageIds: [],
         verboseOutput: process.env.WORDPRESS_VERBOSE_OUTPUT === "true",
         perPage: 100,
-        searchAndReplaceContentUrls: {
-          sourceUrl: "https://content.yogaposies.com",
-          replacementUrl: "https://content.yogaposies.com",
-        },
         auth: {
           wpcom_user: process.env.WORDPRESS_USER,
           wpcom_pass: process.env.WORDPRESS_PASSWORD,
         },
-        // Set how many simultaneous requests are sent at once.
-        concurrentRequests: 10,
-        includedRoutes: [
-          "**/categories",
-          "**/posts",
-          "**/pages",
-          "**/media",
-          "**/tags",
-          "**/taxonomies",
-        ],
-        // Blacklisted routes using glob patterns
-        excludedRoutes: [],
-        keepMediaSizes: false,
-        // use a custom normalizer which is applied after the built-in ones.
-        normalizer: function({ entities }) {
-          return entities
-        },
-      },
-    },
-    /* {
-      resolve: "gatsby-source-tribe-events",
-      options: {
-        baseUrl: "https://content.yogaposies.com/",
-        maxEvents: 5,
-      },
-    }, */
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "UA-75170508-2",
-      },
-    },
-
-    {
-      resolve: "gatsby-plugin-robots-txt",
-      options: {
-        host: "https://yogaposies.com",
-        sitemap: "https://yogaposies.com/sitemap.xml",
-        policy: [{ userAgent: "*", allow: "/" }],
-      },
-    },
-    {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
-      options: {
-        fonts: [
-          {
-            family: `Montserrat`,
-          },
-        ],
       },
     },
   ],
--- src/templates/Page.js
+++ src/templates/Page.js
@@ -3,20 +3,43 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const PageTemplate = ({ data }) => (
-  <Layout>
-    <SEO
-      title={data.wordpressPage.title}
-      description={data.wordpressPage.excerpt}
-    />
-    <div className={data.wordpressPage.slug}>
-      <h2>{data.wordpressPage.title}</h2>
-      <div
-        dangerouslySetInnerHTML={{ __html: data.wordpressPage.content }}
-      ></div>
-    </div>
-  </Layout>
-)
+// const PageTemplate = ({ data }) => (
+//   <Layout>
+//     <SEO
+//       title={data.wordpressPage.title}
+//       description={data.wordpressPage.excerpt}
+//     />
+//     <div className={data.wordpressPage.slug}>
+//       <h2>{data.wordpressPage.title}</h2>
+//       <div
+//         dangerouslySetInnerHTML={{ __html: data.wordpressPage.content }}
+//       ></div>
+//     </div>
+//   </Layout>
+// )
+
+const PageTemplate = ({ data: { previous, next, post } }) => {
+  return (
+    <Layout>
+      ...
+      <h1 itemProp="headline">{parse(post.title)}</h1>
+      <p>{post.date}</p>
+      {/* Checking for and adding the post excerpt if the current post has one*/}
+      {post.excerpt && (
+        <div className="post-excerpt">{parse(post.excerpt)}</div>
+      )}
+      {/* if we have a featured image for this post let's display it */}
+      {featuredImage?.fluid && (
+        <Image
+          fluid={featuredImage.fluid}
+          alt={featuredImage.alt}
+          style={{ marginBottom: 50 }}
+        />
+      )}
+      ...
+    </Layout>
+  )
+}
 
 export default PageTemplate
 
