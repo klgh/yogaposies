b6a9135cd87d4c443c9c0710cc8b0c0e5b488362
Kaleigh Scruggs <kaleighscruggs@gmail.com>
2022-10-26 18:44:53 -0700

    updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     description: `Yoga Classes in the metro Atlanta area including Dunwoody, Roswell, and Sandy Springs. Classes are geared towards anyone and everyone including kids, pre-teens, teens, adults, and prenatal yoga.`,
     author: `@klgh`,
     siteUrl: "https://yogaposies.com",
-    shortDescription: `Our goal at Yoga Posies is to introduce yoga in a clear and simple way that makes it fun to do.`
+    shortDescription: `Our goal at Yoga Posies is to introduce yoga in a clear and simple way that makes it fun to do.`,
   },
   plugins: [
     `gatsby-plugin-sitemap`,
@@ -13,8 +13,8 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`
-      }
+        path: `${__dirname}/src/images`,
+      },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -28,10 +28,43 @@ module.exports = {
         background_color: `#663399`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
-      }
-    }
-  ]
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: "gatsby-plugin-a11y-report",
+      options: {
+        showInProduction: false,
+        toastAutoClose: false,
+        query: `
+          {
+            allSitePage(
+              filter: {
+                path: { regex: "/^(?!/404/|/404.html|/dev-404-page/)/" }
+              }
+            ) {
+              edges {
+                node {
+                  path
+                }
+              }
+            }
+          }
+        `,
+        ignoreCheck: ["/404*", "/tag/*"],
+        serverOptions: {
+          host: "localhost",
+          port: "8341",
+        },
+        axeOptions: {
+          locale: "ja",
+        },
+        loggingOptions: {
+          result: ["violations", "incomplete"],
+        },
+      },
+    },
+  ],
 }
